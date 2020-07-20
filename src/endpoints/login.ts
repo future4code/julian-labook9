de1bb770c6bd64fcaab165c917023cacb473a470
Beatriz Mattos <venganzakills@gmail.com>
2020-07-20 16:26:17 -0300

    Endpoint login

---
+++ src/endpoints/login.ts
@@ -0,0 +1,50 @@
+import express, { Request, Response } from "express";
+import { UserDatabase } from "../data/UserDatabase";
+import HashManager from "../services/HashManager";
+import { Authenticator } from "../services/Authenticator";
+import { BaseDataBase } from "../data/BaseDatabase";
+
+/* Login de usuário criado - POST => /login */
+export const loginEndpoint = async (req: Request, res: Response) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const role = req.body.role;
+
+        const userDataBase = new UserDatabase();
+        const user = await userDataBase.getUserByEmail(email);
+
+        const hashManager = new HashManager();
+
+        /* Validação senha */
+        const isPasswordCorrect = await hashManager.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            throw new Error("Usuário ou senha inválidos")
+        };
+
+        /* Validação email */
+        if (!email || email.indexOf("@") === -1) {
+            throw new Error("Email inválido")
+        };
+
+        /* Validação campos */
+        if (!email || !password) {
+            throw new Error("Parâmetros inválidos")
+        };
+
+        const authenticator = new Authenticator();
+
+        const token = authenticator.generateToken({
+            id: user.id,
+            role: role
+        });
+
+        res.status(200).send({ access_token: token });
+
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    };
+
+    await BaseDataBase.destroyConnection();
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
+
+import { loginEndpoint } from "./endpoints/login";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+/* path dos endpoints -- manter aqui temporariamente */
+app.post("/login", loginEndpoint);
 
 /* Configurações do express para iniciar o servidor */
 const server = app.listen(process.env.PORT || 3003, () => {
