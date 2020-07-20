f8b5c4c8ef5959cf5a1ceb6f9ac4ae78ce50c112
Beatriz Mattos <venganzakills@gmail.com>
2020-07-20 15:04:15 -0300

    instalando dependências e adicionando arquivos base

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+/* Configurações do express para iniciar o servidor */
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    }
+    else {
+        console.error(`Failure upon starting server.`);
+    }
+});
