b2ba4f9667e664caaa2e63ad1c2a2f893ad6afe3
Beatriz Mattos <venganzakills@gmail.com>
2020-07-20 21:05:31 -0300

    endpoint add-friend e undo-friendship

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { AddressInfo } from "net";
 
 import { loginEndpoint } from "./endpoints/login";
+import { addFriendEndpoint } from "./endpoints/addFriend";
+import { undoFriendshipEndpoint } from "./endpoints/undoFriendship";
 
 dotenv.config();
 
@@ -11,7 +13,10 @@ const app = express();
 app.use(express.json());
 
 /* path dos endpoints -- manter aqui temporariamente */
+
 app.post("/login", loginEndpoint);
+app.post("/add-friend", addFriendEndpoint);
+app.post("/undo-friendship", undoFriendshipEndpoint);
 
 /* Configurações do express para iniciar o servidor */
 const server = app.listen(process.env.PORT || 3003, () => {
