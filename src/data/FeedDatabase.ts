07c0d9b517e3ad525fb632b59d90ffbc9a01b518
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-07-22 13:32:54 -0300

    ajustes em FeedDatabase

--- src/data/FeedDatabase.ts
+++ src/data/FeedDatabase.ts
@@ -7,7 +7,7 @@ export class FeedDatabase extends BaseDatabase {
             SELECT Posts.posts_id, photo, description, creation_date, type, User.id, User.name
             FROM Posts
             JOIN users_relation
-            ON users_relation.user_to_follow_id = Posts.user_id 
+            ON users_relation.user_add_friend_by_id = Posts.user_id 
             AND users_relation.user_id = '${userId}'
             JOIN User
             ON Posts.user_id = User.id; 
